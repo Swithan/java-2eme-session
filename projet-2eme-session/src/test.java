36cc339ef6c195b3f1dd0e4261dfcd15945d10e8
natha <natha@WINDELL-MPKD2DF.lan>
2020-07-24 13:00:47 +0200

    creation du projet

---
+++ projet-2eme-session/src/test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author natha
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
