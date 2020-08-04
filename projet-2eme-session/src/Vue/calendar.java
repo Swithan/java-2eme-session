bbddc96ab5a4e5fbb0e8b5ada9d14977c44401fe
natha <natha@WINDELL-MPKD2DF>
2020-08-04 12:17:16 +0200

    first listener

--- projet-2eme-session/src/Vue/calendar.java
+++ projet-2eme-session/src/Vue/calendar.java
@@ -1,31 +1,61 @@
 package Vue;
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.EventListener;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 import Model.database;
 
-public class calendar extends JFrame{
+public class calendar extends JFrame implements ActionListener{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4131581731669920940L;
 
+	static JButton presence ;
+
+	static JButton competition;
+
+	static JButton membres;
+	
+	static JLabel l = new JLabel("nothing selected"); 
 	private JToolBar createToolBar () {
 		JToolBar bar = new JToolBar ();
 		
-		JButton presence = new JButton("Présences");
+		presence = new JButton("Présences");
 		bar.add(presence);
 		
-		JButton competition = new JButton("Compétitions");
+		competition = new JButton("Compétitions");
 		bar.add(competition);
 		
-		JButton membres = new JButton("Gérer les membres");
-		bar.add(membres);
-		
+		membres = new JButton("Gérer les membres");
+		bar.add(membres);		
 		return bar;
+	}
+	
+	private JPanel presence () {
+		database db = new database();
+		DefaultTableModel data = db.getData("nom as \"Nom\", prenom as \"Prénom\", groupe as \"Groupe\", presence as \"Présence\", motif as \"Motif\"", "membres join presence on membres.id = presence.nageur", "");
+		
+		JTable t = new JTable ();
+		t.setModel(data);
+		t.isCellEditable(3, 1);
+		
+		JButton b = new JButton();
+		b.setText("Valider");
+		b.setSize(10, 5);
+		
+		JPanel contentPane = (JPanel) getContentPane();
+		contentPane.add(createToolBar(), BorderLayout.NORTH);
+
+		contentPane.add(t);
+		contentPane.add(b, BorderLayout.SOUTH);
+		
+		return contentPane;
 	}
 	
 	public calendar () {
@@ -34,21 +64,22 @@ public class calendar extends JFrame{
 		this.setSize(600, 400);
 		this.setLocationRelativeTo(null);
 		
-		database db = new database();
-		DefaultTableModel data = db.getData("nom as \"Nom\", prenom as \"Prénom\", groupe as \"Groupe\", presence as \"Présence\", motif as \"Motif\"", "membres join presence on membres.id = presence.nageur", "");
+		presence();
 		
-		JTable t = new JTable ();
-		t.setModel(data);
-				
-		JPanel contentPane = (JPanel) getContentPane();
-		contentPane.add(createToolBar(), BorderLayout.NORTH);
-
-		contentPane.add(t);
-
 	}
 	
 	public static void main(String[] args) {
 		calendar calendar = new calendar();
+		
+		presence.addActionListener(calendar);
+		competition.addActionListener(calendar);
+		membres.addActionListener(calendar);
+
 		calendar.setVisible(true);
 	}
+	
+	public void actionPerformed(ActionEvent e) 
+    { 
+        l.setText(e.getActionCommand() + " selected."); 
+    } 
 }
