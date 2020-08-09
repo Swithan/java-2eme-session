f16ec3cc9743b38defc2f608fd512966666d9bd6
natha <natha@WINDELL-MPKD2DF.lan>
2020-08-09 14:03:44 +0200

    table modif

--- projet-2eme-session/src/Vue/GUI.java
+++ projet-2eme-session/src/Vue/GUI.java
@@ -1,24 +1,23 @@
 package Vue;
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Date;
-import java.util.EventListener;
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 import Model.Database;
 
-public class GUI extends JFrame implements ActionListener{
+public class GUI extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = -4131581731669920940L;
 	
 	public JFrame window = new JFrame("Natation");
 	public JLabel club = new JLabel();
+	public JTable dataTable = new JTable();
+	public Date today = new Date();
+	public JLabel left = new JLabel();
 
-	
 	//Barre de menu
 	public JMenuBar bar = new JMenuBar();
 	public JMenuItem presence = new JMenuItem("Présences");
@@ -29,8 +28,6 @@ public class GUI extends JFrame implements ActionListener{
 	public JLabel date = new JLabel();
 	public JButton dateBefore = new JButton("<--");
 	public JButton dateNext = new JButton("-->");
-	
-	public JTable dataMembres = new JTable();
 	public JButton sendPresences = new JButton("Valider");
 	
 	//competitions
@@ -50,57 +47,75 @@ public class GUI extends JFrame implements ActionListener{
 		
 		//Barre de menu
 		
-		createToolBar();
+		createMenuBar();
 		
-		//Présences
-		
-		presence(db);
-		if (presence.isSelected()) {
-			System.out.println("pres");
-		} else {
-			club.setText("Logo du club ici");
-			club.setBounds(0, 25, 500, 475);
-		
-			window.add(club);
-		}
-
+		//Page d'accueil
+		club.setText("Logo du club ici");
+		club.setBounds(0, 25, 500, 475);
+		window.add(club);
 	}
 	
 	//Creer la barre de menu
-	private void createToolBar () {
+	private void createMenuBar () {
 		presence.addActionListener(this);
-		//presence.setBorder(null);
 		competition.addActionListener(this);
-		//competition.setBorder(null);
 		membres.addActionListener(this);
-		//membres.setBorder(null);
 		
 		bar.setBounds(0, 0, 500, 25);
 		bar.add(presence);
 		bar.add(competition);
 		bar.add(membres);
-		window.add(bar);
+		window.setJMenuBar(bar);
+		
 	}
 	
 	private void presence (Database db) {
 		
-		DefaultTableModel data = db.getData("nom as \"Nom\", prenom as \"Prénom\", groupe as \"Groupe\", presence as \"Présence\", motif as \"Motif\"", "membres join presence on membres.id = presence.nageur", "");
+		DefaultTableModel data = db.getData("nom as \"Nom\", prenom as \"Prénom\", groupe as \"Groupe\", present as \"Présence\", motif as \"Motif\"", "membres join presence on membres.id = presence.nageur", "");
+				
+		dataTable.setModel(data);
+		dataTable.setName("Presences");
+		dataTable.setBounds(0, 25, 500, 395);
 		
-		dataMembres.setModel(data);
+		sendPresences.setBounds(0, 420, 500, 25);
 		
+		window.remove(club);
+		window.setVisible(false);
+		changeDate();
+		window.add(dataTable);
+		window.add(sendPresences);
+		window.setVisible(true);
 	}
 	
+	private void competition(Database db) {
+		
+		DefaultTableModel data = db.getData("*", "competition", "");	
+		
+		window.setVisible(false);
+		
+		//dataTable = null;
+		dataTable.setModel(data);		
+		//dataTable = new JTable(data, names);
+		dataTable.setName("Presences");
+		dataTable.setBounds(0, 25, 500, 395);
+		
+		window.remove(club);
+		window.remove(sendPresences);
+		window.remove(left);
+		window.add(dataTable);
+		window.setVisible(true);
+	}
+
 	public GUI () {
 		super();	
 	}
-	
+
 	public void createInterface (String[] args) {
 		GUI calendar = new GUI();
 		
 		presence.addActionListener(calendar);
 		competition.addActionListener(calendar);
 		membres.addActionListener(calendar);
-		changeDate();
 
 		calendar.setVisible(true);
 	}
@@ -108,34 +123,44 @@ public class GUI extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) 
     { 
-		System.out.println(presence.isSelected());
-		if (presence.isSelected()) {
-			System.out.println("coco");
-			
-		} else if (competition.isSelected()) {
-			System.out.println("cece");
-		} else {
+		Database db = new Database();
+		if ("Présences" == e.getActionCommand()) {
+			presence(db);
+		} else if ("Compétitions" == e.getActionCommand()) {
+			competition(db);
+		} else if ("Membres" == e.getActionCommand()){
 			System.out.println(e.getActionCommand() + " selected."); 
+		} else if ("<--" == e.getActionCommand()) {
+			today.setDate(today.getDate() - 1);
+			window.setVisible(false);
+			window.remove(left);
+			left.setText(today.toLocaleString());
+			window.add(left);
+			window.setVisible(true);
+		} else if ("-->" == e.getActionCommand()) {
+			today.setDate(today.getDate() + 1);
+			window.setVisible(false);
+			window.remove(left);
+			left.setText(today.toLocaleString());
+			window.add(left);
+			window.setVisible(true);
 		}
     }
 
-	private void competition() {
-		Database db = new Database();
-		DefaultTableModel data = db.getData("*", "competition", "");		
-		JTable table = new JTable();
-		table.setModel(data);
-	}
-
 	@SuppressWarnings("deprecation")
-	private static void changeDate() {
-		Date today = new Date();
-		
-		JLabel left = new JLabel();
-		
+	private void changeDate() {				
 		JButton past = new JButton("<--");
+		past.setBounds(300, 0, 100, 25);
 		JButton future = new JButton("-->");
-		
+		future.setBounds(400, 0, 100, 25);
+
 		left.setText(today.toLocaleString());
+		past.addActionListener(this);
+		left.add(past);
+		future.addActionListener(this);
+		left.add(future);
+		left.setBounds(0, 0, 500, 25);
+		window.add(left);
 	} 
 	
 	public static void main (String[] args) {
